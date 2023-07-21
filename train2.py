1927f37adc9160f51cf09aecee4a20495f15140e
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-07-21 17:40:49 +0800

    Update train2.py

--- train2.py
+++ train2.py
@@ -15,8 +15,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow import keras
 
-from models.cls_msg_model import CLS_MSG_Model
-from models.cls_ssg_model import CLS_SSG_Model
 
 tf.random.set_seed(1234)
 
@@ -241,7 +239,122 @@ print('[debug] - >>NUM_CLASSES', NUM_CLASSES)
 print('[debug] - >>BATCH_SIZE', BATCH_SIZE)
 print('[debug] - >>NUM_POINTS', NUM_POINTS)
 
+#################################################################################
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 
+from pnet2_layers.layers import Pointnet_SA, Pointnet_SA_MSG
+
+
+
+
+class CLS_MSG_Model(Model):
+
+	def __init__(self, batch_size, num_classes, bn=False, activation=tf.nn.relu):
+		super(CLS_MSG_Model, self).__init__()
+
+		self.activation = activation
+		self.batch_size = batch_size
+		self.num_classes = num_classes
+		self.bn = bn
+		self.keep_prob = 0.4
+
+		self.kernel_initializer = 'glorot_normal'
+		self.kernel_regularizer = None
+
+		self.init_network()
+
+
+	def init_network(self):
+
+		self.layer1 = Pointnet_SA_MSG(
+			npoint=1024,
+			radius_list=[0.1,0.2,0.4],
+			nsample_list=[16,32,128],
+			mlp=[[32,32,64], [64,64,128], [64,96,128]],
+			activation=self.activation,
+			bn = self.bn
+		)
+
+		self.layer2 = Pointnet_SA_MSG(
+			npoint=512,
+			radius_list=[0.2,0.4,0.8],
+			nsample_list=[32,64,128],
+			mlp=[[64,64,128], [128,128,256], [128,128,256]],
+			activation=self.activation,
+			bn = self.bn
+		)
+
+		self.layer3 = Pointnet_SA(
+			npoint=None,
+			radius=None,
+			nsample=None,
+			mlp=[256, 512, 1024],
+			group_all=True,
+			activation=self.activation,
+			bn = self.bn
+		)
+
+		self.dense1 = Dense(512, activation=self.activation)
+		self.dropout1 = Dropout(self.keep_prob)
+
+		self.dense2 = Dense(128, activation=self.activation)
+		self.dropout2 = Dropout(self.keep_prob)
+
+		self.dense3 = Dense(self.num_classes, activation=tf.nn.softmax)
+
+
+	def forward_pass(self, input, training):
+
+		xyz, points = self.layer1(input, None, training=training)
+		xyz, points = self.layer2(xyz, points, training=training)
+		xyz, points = self.layer3(xyz, points, training=training)
+
+		net = tf.reshape(points, (self.batch_size, -1))
+
+		net = self.dense1(net)
+		net = self.dropout1(net)
+
+		net = self.dense2(net)
+		net = self.dropout2(net)
+
+		pred = self.dense3(net)
+
+		return pred
+
+
+	def train_step(self, input):
+
+		with tf.GradientTape() as tape:
+
+			pred = self.forward_pass(input[0], True)
+			loss = self.compiled_loss(input[1], pred)
+		
+		gradients = tape.gradient(loss, self.trainable_variables)
+		self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+		self.compiled_metrics.update_state(input[1], pred)
+
+		return {m.name: m.result() for m in self.metrics}
+
+
+	def test_step(self, input):
+
+		pred = self.forward_pass(input[0], False)
+		loss = self.compiled_loss(input[1], pred)
+
+		self.compiled_metrics.update_state(input[1], pred)
+
+		return {m.name: m.result() for m in self.metrics}
+
+
+	def call(self, input, training=False):
+
+		return self.forward_pass(input, training)
+
+
+#################################################################################
 
 model = CLS_MSG_Model(config['batch_size'], NUM_CLASSES, config['bn'])
 
@@ -280,4 +393,95 @@ print('>>>>>>>>>>>>>>>>>>>>>>>>evaluate<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
 test_loss, test_acc = model.evaluate(test_dataset, verbose=2)
 print('[debug] - Test loss:', test_loss)
 print('[debug] - Test accuracy:', test_acc)
+
+
+print('>>>>>>>>>>>>>>>>>>>>>>>>predict 1<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+
+data = test_dataset.take(1)
+print('data:', test_dataset)
+
+points, labels = list(data)[0]
+
+print('points shape:', points.shape)
+print('labels shape:', labels.shape)
+print('labels:', labels)
+
+
+# run test data through model
+preds = model.predict(points)
+#print('>>preds:', preds)
+predicts = tf.math.argmax(preds, -1)
+print('predicts:', predicts)
+
+
+
+
+print('>>>>>>>>>>>>>>>>>>>>>>>>predict 2<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+
+def get_batch_model(model, batch_size=1):
+    n_batch = batch_size
+    print('[debug] - model batch size ', n_batch)
+    model2 = PointConvModel(n_batch, config['bn'], num_classes=NUM_CLASSES)
+    weights = model.get_weights()
+    model2.build((n_batch, NUM_POINTS, 3))
+    '''
+    model2.compile(
+        optimizer=keras.optimizers.Adam(config['lr']),
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()]
+    )
+    '''
+    model2.set_weights(weights)
+    return model2
+    
+example_images1 = []
+example_labels1 = []
+
+#获取10个batch的数据
+for batch in test_dataset:
+    example_images1.append(batch[0].numpy())
+    example_labels1.append(batch[1].numpy())
+
+example_images1 = np.concatenate(example_images1, axis = 0)    
+example_labels1 = np.concatenate(example_labels1, axis = 0)   
+print('[debug] - >>example_images1 shape', example_images1.shape)
+print('[debug] - >>example_labels1 shape', example_labels1.shape)
+print('[debug] - >>example_images1 shape[0]', example_images1.shape[0])
+
+print('[debug] - >>example_images1[0] shape', example_images1[0].shape)
+print('[debug] - >>example_labels1[0] shape', example_labels1[0].shape)
+
+
+
+print('[debug] - >>start single predict------------------------------------------')
+
+example_images = []
+example_labels = []
+
+#获取10个batch的数据
+for batch in test_dataset:
+    example_images.append([batch[0].numpy()[0]])
+    example_labels.append([batch[1].numpy()[0]])
+    break
+
+example_images = np.concatenate(example_images, axis = 0)    
+example_labels = np.concatenate(example_labels, axis = 0)    
+
+print('[debug] - >>example_images shape', example_images.shape)
+print('[debug] - >>example_labels shape', example_labels.shape)
+print('[debug] - >>example_images shape[0]', example_images.shape[0])
+
+print('[debug] - >>example_images[0] shape', example_images[0].shape)
+print('[debug] - >>example_labels[0] shape', example_labels[0].shape)
+
+print('[debug] - >>example_labels', example_labels)
+
+n_batch = example_images.shape[0]
+model2 = get_batch_model(model, n_batch)
+Y_pred = model2.predict(example_images, batch_size = n_batch)
+print('[debug] - >>n_batch', n_batch)
+with printoptions(precision=6, suppress=True):
+    print('[debug] - >>Y_pred', Y_pred)
+Y_pred = tf.math.argmax(Y_pred, -1)
+print('[debug] - predicts:', Y_pred)
 
