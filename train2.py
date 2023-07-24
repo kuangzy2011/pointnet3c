6b49c38b17c44d5e02a8b2485ec7fc80588cbd62
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-07-24 14:53:23 +0800

    Update train2.py

--- train2.py
+++ train2.py
@@ -250,108 +250,89 @@ from pnet2_layers.layers import Pointnet_SA, Pointnet_SA_MSG
 
 
 class CLS_MSG_Model(Model):
+    def __init__(self, batch_size, num_classes, bn=False, activation=tf.nn.relu):
+	super(CLS_MSG_Model, self).__init__()
+        print('[debug-CLS_MSG_Model-init] -----------------------------')
+	self.activation = activation
+	self.batch_size = batch_size
+	self.num_classes = num_classes
+	self.bn = bn
+	self.keep_prob = 0.4
 
-	def __init__(self, batch_size, num_classes, bn=False, activation=tf.nn.relu):
-		super(CLS_MSG_Model, self).__init__()
+	self.kernel_initializer = 'glorot_normal'
+	self.kernel_regularizer = None
+	self.init_network()
 
-		self.activation = activation
-		self.batch_size = batch_size
-		self.num_classes = num_classes
-		self.bn = bn
-		self.keep_prob = 0.4
+    def init_network(self):
+        print('[debug-CLS_MSG_Model-init_network] -----------------------------')
+	self.layer1 = Pointnet_SA_MSG(
+	    npoint=1024,
+	    radius_list=[0.1,0.2,0.4],
+	    nsample_list=[16,32,128],
+	    mlp=[[32,32,64], [64,64,128], [64,96,128]],
+	    activation=self.activation,
+    	    bn = self.bn
+	)
 
-		self.kernel_initializer = 'glorot_normal'
-		self.kernel_regularizer = None
+	self.layer2 = Pointnet_SA_MSG(
+	    npoint=512,
+	    radius_list=[0.2,0.4,0.8],
+	    nsample_list=[32,64,128],
+	    mlp=[[64,64,128], [128,128,256], [128,128,256]],
+	    activation=self.activation,
+	    bn = self.bn
+	)
 
-		self.init_network()
+	self.layer3 = Pointnet_SA(
+	    npoint=None,
+	    radius=None,
+	    nsample=None,
+	    mlp=[256, 512, 1024],
+	    group_all=True,
+	    activation=self.activation,
+	    bn = self.bn
+	)
 
+	self.dense1 = Dense(512, activation=self.activation)
+	self.dropout1 = Dropout(self.keep_prob)
+	self.dense2 = Dense(128, activation=self.activation)
+	self.dropout2 = Dropout(self.keep_prob)
+	self.dense3 = Dense(self.num_classes, activation=tf.nn.softmax)
+    
+    def forward_pass(self, input, training):
+        print('[debug-CLS_MSG_Model-forward_pass] -----------------------------')
+	xyz, points = self.layer1(input, None, training=training)
+	xyz, points = self.layer2(xyz, points, training=training)
+	xyz, points = self.layer3(xyz, points, training=training)
+	net = tf.reshape(points, (self.batch_size, -1))
+	net = self.dense1(net)
+	net = self.dropout1(net)
+	net = self.dense2(net)
+ 	net = self.dropout2(net)
+	pred = self.dense3(net)
+	return pred
 
-	def init_network(self):
-
-		self.layer1 = Pointnet_SA_MSG(
-			npoint=1024,
-			radius_list=[0.1,0.2,0.4],
-			nsample_list=[16,32,128],
-			mlp=[[32,32,64], [64,64,128], [64,96,128]],
-			activation=self.activation,
-			bn = self.bn
-		)
-
-		self.layer2 = Pointnet_SA_MSG(
-			npoint=512,
-			radius_list=[0.2,0.4,0.8],
-			nsample_list=[32,64,128],
-			mlp=[[64,64,128], [128,128,256], [128,128,256]],
-			activation=self.activation,
-			bn = self.bn
-		)
-
-		self.layer3 = Pointnet_SA(
-			npoint=None,
-			radius=None,
-			nsample=None,
-			mlp=[256, 512, 1024],
-			group_all=True,
-			activation=self.activation,
-			bn = self.bn
-		)
-
-		self.dense1 = Dense(512, activation=self.activation)
-		self.dropout1 = Dropout(self.keep_prob)
-
-		self.dense2 = Dense(128, activation=self.activation)
-		self.dropout2 = Dropout(self.keep_prob)
-
-		self.dense3 = Dense(self.num_classes, activation=tf.nn.softmax)
-
-
-	def forward_pass(self, input, training):
-
-		xyz, points = self.layer1(input, None, training=training)
-		xyz, points = self.layer2(xyz, points, training=training)
-		xyz, points = self.layer3(xyz, points, training=training)
-
-		net = tf.reshape(points, (self.batch_size, -1))
-
-		net = self.dense1(net)
-		net = self.dropout1(net)
-
-		net = self.dense2(net)
-		net = self.dropout2(net)
-
-		pred = self.dense3(net)
-
-		return pred
-
-
-	def train_step(self, input):
-
-		with tf.GradientTape() as tape:
-
-			pred = self.forward_pass(input[0], True)
-			loss = self.compiled_loss(input[1], pred)
+    def train_step(self, input):
+        print('[debug-CLS_MSG_Model-train_step] -----------------------------')
+	with tf.GradientTape() as tape:
+	    pred = self.forward_pass(input[0], True)
+	    loss = self.compiled_loss(input[1], pred)
 		
-		gradients = tape.gradient(loss, self.trainable_variables)
-		self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+	gradients = tape.gradient(loss, self.trainable_variables)
+	self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+	self.compiled_metrics.update_state(input[1], pred)
+	return {m.name: m.result() for m in self.metrics}
 
-		self.compiled_metrics.update_state(input[1], pred)
+    def test_step(self, input):
+        print('[debug-CLS_MSG_Model-test_step] -----------------------------')
+	pred = self.forward_pass(input[0], False)
+	loss = self.compiled_loss(input[1], pred)
+	self.compiled_metrics.update_state(input[1], pred)
+	return {m.name: m.result() for m in self.metrics}
 
-		return {m.name: m.result() for m in self.metrics}
-
-
-	def test_step(self, input):
-
-		pred = self.forward_pass(input[0], False)
-		loss = self.compiled_loss(input[1], pred)
-
-		self.compiled_metrics.update_state(input[1], pred)
-
-		return {m.name: m.result() for m in self.metrics}
-
-
-	def call(self, input, training=False):
-
-		return self.forward_pass(input, training)
+    def call(self, input, training=False):
+        print('[debug-CLS_MSG_Model-call] -----------------------------')
+	return self.forward_pass(input, training)
 
 
 #################################################################################
@@ -372,7 +353,9 @@ callbacks = [
 	keras.callbacks.ModelCheckpoint('./logs/{}/model/weights.ckpt'.format(config['log_dir']), 'val_sparse_categorical_accuracy', save_best_only=True)
 ]
 
+print('[debug] - ------------------build before--------------------')
 model.build(input_shape=(config['batch_size'], NUM_POINTS, 3))
+print('[debug] - ------------------build after--------------------')
 print(model.summary())
 
 model.compile(
