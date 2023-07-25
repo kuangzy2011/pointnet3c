ac40c08ea541101cc4b6d5dbe1ff0edbc454513a
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-07-25 13:55:30 +0800

    Update train2.py

--- train2.py
+++ train2.py
@@ -350,12 +350,12 @@ model = CLS_MSG_Model(config['batch_size'], NUM_CLASSES, config['bn'])
 print('[debug] - ------------------build before--------------------config[batch_size]', config['batch_size'], ', NUM_POINTS', NUM_POINTS)
 model.build(input_shape=(config['batch_size'], NUM_POINTS, 3))
 print('[debug] - ------------------build after--------------------')
-model._set_inputs(tf.keras.Input(shape=[None, NUM_POINTS, 3]))
+#model._set_inputs(tf.keras.Input(shape=[None, NUM_POINTS, 3]))
 
 callbacks = [
 	keras.callbacks.EarlyStopping('val_sparse_categorical_accuracy', min_delta=0.01, patience=10),
 	keras.callbacks.TensorBoard('./logs/{}'.format(config['log_dir']), update_freq=50),
-	keras.callbacks.ModelCheckpoint('./logs/{}/model/weights.ckpt'.format(config['log_dir']), 'val_sparse_categorical_accuracy', save_best_only=True)
+	#keras.callbacks.ModelCheckpoint('./logs/{}/model/weights.ckpt'.format(config['log_dir']), 'val_sparse_categorical_accuracy', save_best_only=True)
 ]
 
 print(model.summary())
